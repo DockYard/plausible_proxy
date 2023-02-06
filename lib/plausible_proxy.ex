b1eabc3d889c860abb824a9e5b5b47fb15322503
TheFirstAvenger <mike@mikebinns.net>
2023-02-06 10:41:27 -0500

    mix new plausible_proxy --sup

---
+++ lib/plausible_proxy.ex
@@ -0,0 +1,18 @@
+defmodule PlausibleProxy do
+  @moduledoc """
+  Documentation for `PlausibleProxy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlausibleProxy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
